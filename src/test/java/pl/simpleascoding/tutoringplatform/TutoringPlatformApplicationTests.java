3ea4b87b00fab43eb0be32695c6f3add0d14ccea
MateuszCiapala <ciapek94@gmail.com>
2022-06-22 03:26:28 +0200

    Basic Configuration -pom.xml dependencies added -application.properties configured

---
+++ src/test/java/pl/simpleascoding/tutoringplatform/TutoringPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.simpleascoding.tutoringplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoringPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

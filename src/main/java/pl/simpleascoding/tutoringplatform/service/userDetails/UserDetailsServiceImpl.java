493514ab29514fd441aad89e5a5069b05a24d0a5
Łukasz Konieczny <lukimaluki@gmail.com>
2022-06-27 23:52:45 +0200

    UserDetailsServiceImpl
    - Fixed security implementation by adding back UserDetailsServiceImpl class

---
+++ src/main/java/pl/simpleascoding/tutoringplatform/service/userDetails/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package pl.simpleascoding.tutoringplatform.service.userDetails;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.simpleascoding.tutoringplatform.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User with username \"" + username + "\" not found"));
+    }
+
+}

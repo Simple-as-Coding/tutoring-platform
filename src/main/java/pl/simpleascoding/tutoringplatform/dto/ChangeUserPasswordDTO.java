185b4e2a1d2cf6ea5da31fd917256df450cb35d1
Dawid Bielecki dawciobiel <dawciobiel@gmail.com>
2022-07-06 18:32:47 +0200

    refactor: UserServiceImpl Refactored service method of changing user password Refactored method of verifying password matching Refactored naming of the methods, variables, and arguments Refactored code formatting Added java doc documentation

--- src/main/java/pl/simpleascoding/tutoringplatform/dto/ChangeUserPasswordDTO.java
+++ src/main/java/pl/simpleascoding/tutoringplatform/dto/ChangeUserPasswordDTO.java
@@ -2,6 +2,13 @@ package pl.simpleascoding.tutoringplatform.dto;
 
 import org.hibernate.annotations.Immutable;
 
+/**
+ * Record used to transfer data (passwords) between the service and the controller
+ *
+ * @param oldPassword   The user's existing password
+ * @param newPassword   New user password
+ * @param passwordConfirmed   Confirmation of the new user password
+ */
 @Immutable
-public record ChangeUserPasswordDTO(String oldPassword, String newPassword, String newPasswordReturn) {
+public record ChangeUserPasswordDTO(String oldPassword, String newPassword, String passwordConfirmed) {
 }

b60060b955ce4ed59736c203786137e84da83d3f
Dawid Bielecki dawciobiel <dawciobiel@gmail.com>
2022-07-09 21:13:06 +0200

    docs Set proper value for email address in docker/launch_pgadmin.sh

--- docs/docker/launch_pgadmin.sh
+++ docs/docker/launch_pgadmin.sh
@@ -4,6 +4,6 @@ sudo docker run \
  --name pgadmin \
  -e "POSTGRES_USER=postgres" \
  -e "PGADMIN_DEFAULT_PASSWORD=root" \
- -e "PGADMIN_DEFAULT_EMAIL=dawciobiel@gmail.com" \
+ -e "PGADMIN_DEFAULT_EMAIL=your_regiestered_email_address@gmail.com" \
  -p 5050:80 -d \
  dpage/pgadmin4

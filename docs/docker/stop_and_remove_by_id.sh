821868ad9fcdd9c71c53e0903e65264dbeb8f901
Dawid Bielecki dawciobiel <dawciobiel@gmail.com>
2022-07-09 20:34:30 +0200

    docs Links organization on docker and docker-postgres

---
+++ docs/docker/stop_and_remove_by_id.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+
+function print_usage() {
+  echo "Usage:"
+  echo ""
+  echo "  stop_and_remove_by_id.sh container_id"
+  echo ""
+  echo "container_ids - you can list by executing:"
+  echo ""
+  echo " sudo docker ps -a"
+}
+
+function list_docker_containers () {
+    sudo docker ps -a
+}
+
+function stop_container () {
+    sudo docker stop $1
+}
+
+function remove_container () {
+    sudo docker rm $1
+}
+
+function quit {
+    exit
+}
+
+
+# if [ -n "$1" ]; then
+if [ -z "$1" ]; then
+  print_usage
+  list_docker_containers
+else
+  stop_container
+  remove_container
+fi
+exit
+
+

79deba7099794dc08a5c8b5e75de90fb96e36df9
MateuszCiapala <ciapek94@gmail.com>
2022-08-06 21:52:39 +0200

    refactor: Tutoring package -Removed Tutoring Facade, Service and its parent package as it has no usage

--- src/main/java/pl/simpleascoding/tutoringplatform/service/tutoring/TutoringServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package pl.simpleascoding.tutoringplatform.service.tutoring;
-
-import org.springframework.stereotype.Service;
-
-@Service
-class TutoringServiceImpl implements TutoringService {
-    //todo
-}

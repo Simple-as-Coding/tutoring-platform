b3f29db314b6af3f28d284f2f13dea991c1ba536
Mateusz Bednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2022-08-12 18:27:30 +0200

    changed message variable name in UserIsAlreadyATeacherException class

--- src/main/java/pl/simpleascoding/tutoringplatform/exception/UserIsAlreadyATeacherException.java
+++ src/main/java/pl/simpleascoding/tutoringplatform/exception/UserIsAlreadyATeacherException.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.CONFLICT)
 public class UserIsAlreadyATeacherException extends IllegalArgumentException {
 
-    private static final String MESSAGE_USER_ALREADY_A_TEACHER = "User is already a teacher";
+    private static final String MESSAGE_USER_ALREADY_IS_A_TEACHER = "User is already a teacher";
 
     public UserIsAlreadyATeacherException() {
-        super(MESSAGE_USER_ALREADY_A_TEACHER);
+        super(MESSAGE_USER_ALREADY_IS_A_TEACHER);
     }
 }

2a350dcbddfb1d3cb5ac77efaee41046c40957c6
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-08-22 22:09:13 +0200

    Change TokenType value
    -Changed from CHANGE_EMAIL_CONFIRMATION to CHANGE_PASSWORD_CONFIRMATION

--- src/main/java/pl/simpleascoding/tutoringplatform/domain/token/TokenType.java
+++ src/main/java/pl/simpleascoding/tutoringplatform/domain/token/TokenType.java
@@ -1,5 +1,5 @@
 package pl.simpleascoding.tutoringplatform.domain.token;
 
 public enum TokenType {
-    REGISTER_CONFIRMATION, CHANGE_EMAIL_CONFIRMATION
+    REGISTER_CONFIRMATION, CHANGE_PASSWORD_CONFIRMATION
 }

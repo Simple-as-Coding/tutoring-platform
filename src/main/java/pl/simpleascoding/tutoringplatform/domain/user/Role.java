defadc5776cbe7b9c5fc98880e234e49956826d4
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-08-26 15:37:49 +0200

    Refactor Token and Role
    -Added @Enumerated annotation to enum fields in Token and Role

--- src/main/java/pl/simpleascoding/tutoringplatform/domain/user/Role.java
+++ src/main/java/pl/simpleascoding/tutoringplatform/domain/user/Role.java
@@ -15,6 +15,7 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
     private RoleType roleType;
 
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})

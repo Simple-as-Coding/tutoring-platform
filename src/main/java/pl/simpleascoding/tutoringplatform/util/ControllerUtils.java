d3fc58f9f40697508ab6b9556df36cea97451040
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-09-23 15:37:55 +0200

    feat: Create ControllerUtils

---
+++ src/main/java/pl/simpleascoding/tutoringplatform/util/ControllerUtils.java
@@ -0,0 +1,18 @@
+package pl.simpleascoding.tutoringplatform.util;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import pl.simpleascoding.tutoringplatform.dto.RscpDTO;
+
+public class ControllerUtils {
+
+    public static <T> ResponseEntity<T> transformRscpDTOToResponseEntity(RscpDTO<T> rscpDTO) {
+        T body = rscpDTO.body();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("message", rscpDTO.message());
+        HttpStatus httpStatus = HttpStatus.resolve(rscpDTO.status().value());
+
+        return new ResponseEntity<>(body, headers, httpStatus);
+    }
+}

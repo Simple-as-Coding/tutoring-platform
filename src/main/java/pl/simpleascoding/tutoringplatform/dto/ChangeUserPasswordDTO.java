175341dc5341bb0dd546e167a1551365eee4f1a5
Dawid Stużyński <107852058+DawidStuzynski@users.noreply.github.com>
2022-11-14 22:21:10 +0100

    Remove Immutable annotations from DTO records (#73)
    * refactor: Remove Immutable annotations from DTO records
    
    * style: Remove emty line beetwen documentation and code

--- src/main/java/pl/simpleascoding/tutoringplatform/dto/ChangeUserPasswordDTO.java
+++ src/main/java/pl/simpleascoding/tutoringplatform/dto/ChangeUserPasswordDTO.java
@@ -1,14 +1,11 @@
 package pl.simpleascoding.tutoringplatform.dto;
 
-import org.hibernate.annotations.Immutable;
-
 /**
  * Record used to transfer data (passwords) between the service and the controller
  *
- * @param oldPassword   The user's existing password
- * @param newPassword   New user password
- * @param confirmationPassword   Confirmation of the new user password
+ * @param oldPassword          The user's existing password
+ * @param newPassword          New user password
+ * @param confirmationPassword Confirmation of the new user password
  */
-@Immutable
 public record ChangeUserPasswordDTO(String oldPassword, String newPassword, String confirmationPassword) {
 }
--- src/main/java/pl/simpleascoding/tutoringplatform/dto/CreateUserDTO.java
+++ src/main/java/pl/simpleascoding/tutoringplatform/dto/CreateUserDTO.java
@@ -1,7 +1,4 @@
 package pl.simpleascoding.tutoringplatform.dto;
 
-import org.hibernate.annotations.Immutable;
-
-@Immutable
 public record CreateUserDTO(String username, String password, String name, String surname, String email) {
 }

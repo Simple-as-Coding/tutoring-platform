e1a0f8317ae8c3156d862cf835468dac269058ea
xan2301 <2silverghost2@gmail.com>
2023-06-29 16:49:11 +0200

    First commit

--- src/test/java/pl/simpleascoding/tutoringplatform/TutoringPlatformApplicationTests.java
+++ src/test/java/pl/simpleascoding/tutoringplatform/TutoringPlatformApplicationTests.java
@@ -3,6 +3,8 @@ package pl.simpleascoding.tutoringplatform;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
 @SpringBootTest
 class TutoringPlatformApplicationTests {
 
